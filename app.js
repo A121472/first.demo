11a12bcb9d264f6a064d0d0051af919cd35c02a1
Muhammad Amir <muhammedamirpk110@gmail.com>
2026-07-08 10:58:18 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+// app.js
+
+let name = prompt("Enter your name:");
+
+if (name) {
+    alert("Welcome, " + name + "!");
+    console.log("Welcome, " + name + "!");
+} else {
+    alert("Please enter your name.");
+}
